770849890598f6793b940fad720214f528ca3f70
lihongzhen1992 <lihongzhen1992@163.com>
2018-10-28 19:09:05 +0800

    new

--- src/page/organization/workplace/addPlace.vue
+++ src/page/organization/workplace/addPlace.vue
@@ -1,109 +1,123 @@
 <template>
-    <div>
-      <el-dialog :title="title" :visible.sync="dialogFormVisible" @close="dialogClose">
-        <el-form :model="form"  :rules="rules" ref="ruleForm">
-          <el-form-item label="名称" prop="name" :label-width="formLabelWidth" required>
-            <el-input v-model="form.name" auto-complete="off"></el-input>
-          </el-form-item>
-          <el-form-item label="国家/地区" prop="country" :label-width="formLabelWidth" required>
-            <el-select v-model="form.country" placeholder="请选择国家">
-              <el-option label="中国" value="中国"></el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="城市" prop="city" :label-width="formLabelWidth" required>
-            <el-select v-model="form.city" placeholder="请选择城市">
-              <el-option label="深圳" value="深圳"></el-option>
-              <el-option label="广州" value="广州"></el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="详细地址" prop="address" :label-width="formLabelWidth" required>
-            <el-input v-model="form.address" auto-complete="off"></el-input>
-          </el-form-item>
-          <el-form-item label="类型" prop="type" :label-width="formLabelWidth" required>
-            <el-select v-model="form.type" placeholder="请选择类型">
-              <el-option label="注册地址" value="注册地址"></el-option>
-              <el-option label="总部地址" value="总部地址"></el-option>
-              <el-option label="分支办公地址" value="分支办公地址"></el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="描述" :label-width="formLabelWidth">
-            <el-input  type="textarea" :rows="5" v-model="form.describe" auto-complete="off"></el-input>
-          </el-form-item>
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-          <el-button @click="dialogClose">取 消</el-button>
-          <el-button type="primary" @click="submit">确 定</el-button>
-        </div>
-      </el-dialog>
-    </div>
+  <div v-if="dialogFormVisible">
+    <el-dialog :title="title" :visible.sync="dialogFormVisible" @close="dialogClose">
+      <el-form :model="form" :rules="rules" ref="ruleForm">
+        <el-form-item label="名称" prop="name" :label-width="formLabelWidth" required>
+          <el-input v-model="form.name" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="国家/地区" prop="country" :label-width="formLabelWidth" required>
+          <el-select v-model="form.country" placeholder="请选择国家">
+            <el-option label="中国" value="中国"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="城市" prop="city" :label-width="formLabelWidth" required>
+          <el-select v-model="form.city" placeholder="请选择城市">
+            <el-option label="深圳" value="深圳"></el-option>
+            <el-option label="广州" value="广州"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="详细地址" prop="address" :label-width="formLabelWidth" required>
+          <el-input v-model="form.address" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="类型" prop="type" :label-width="formLabelWidth" required>
+          <el-select v-model="form.type" placeholder="请选择类型">
+            <el-option label="注册地址" value="注册地址"></el-option>
+            <el-option label="总部地址" value="总部地址"></el-option>
+            <el-option label="分支办公地址" value="分支办公地址"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="描述" :label-width="formLabelWidth">
+          <el-input type="textarea" :rows="5" v-model="form.describe" auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogClose">取 消</el-button>
+        <el-button type="primary" @click="submit">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "addPlace",
-      data(){
-          return {
-            dialogFormVisible:false,
-            form: {
-              name: '',
-              country: '',
-              city:'',
-              address: '',
-              type: '',
-              describe: '',
-            },
-            formLabelWidth: '120px',
-            rules: {
-              name: [
-                { required: true, message: '请输入活动名称', trigger: 'blur' },
-              ],
-              country: [
-                { required: true, message: '请选择国家/地区', trigger: 'change' }
-              ],
-              city: [
-                { required: true, message: '请选择城市', trigger: 'change' }
-              ],
-              address: [
-                { required: true, message: '请输入详细地址', trigger: 'blur' }
-              ],
-              type: [
-                { required: true, message: '请选择类型', trigger: 'change' }
-              ],
-            }
-          }
-      },
-      methods:{
-        dialogClose(){
-          this.$refs.ruleForm.resetFields();
-          this.dialogFormVisible = false;
-          this.$emit('dialogClose');
+  export default {
+    name: "addPlace",
+    data() {
+      return {
+        dialogFormVisible: false,
+        form: {
+          name: '',
+          country: '',
+          city: '',
+          address: '',
+          type: '',
+          describe: '',
         },
-        submit(){
-          console.log(this.form)
-          // this.$refs.ruleForm.validate((valid) => {
-          //   if (valid) {
-          //     this.$emit('submit',this.form);
-          //     this.dialogFormVisible = false;
-          //     this.$emit('dialogClose');
-          //   } else {
-          //     return false;
-          //   }
-          // });
-          this.$emit('submit',this.form);
-          this.dialogFormVisible = false;
-          this.$emit('dialogClose');
+        formLabelWidth: '120px',
+        rules: {
+          name: [
+            {required: true, message: '请输入活动名称', trigger: 'blur'},
+          ],
+          country: [
+            {required: true, message: '请选择国家/地区', trigger: 'change'}
+          ],
+          city: [
+            {required: true, message: '请选择城市', trigger: 'change'}
+          ],
+          address: [
+            {required: true, message: '请输入详细地址', trigger: 'blur'}
+          ],
+          type: [
+            {required: true, message: '请选择类型', trigger: 'change'}
+          ],
+        }
+      }
+    },
+    methods: {
+      dialogClose() {
+        // this.$refs.ruleForm.resetFields();
+        this.dialogFormVisible = false;
+        this.$emit('dialogClose');
+      },
+      submit() {
+        this.$refs.ruleForm.validate((valid) => {
+          if (valid) {
+            if (this.title == '新增') {
+              this.$emit('submit', this.form, 1);
+            } else if (this.title == '编辑') {
+              this.$emit('submit', this.form, 2);
+            }
+            this.dialogFormVisible = false;
+            this.$emit('dialogClose');
+          } else {
+            return false;
+          }
+        });
+      }
+    },
+    watch: {
+      addShow(val) {
+        this.dialogFormVisible = val;
+        if (val) {//打开弹框初始化
+          this.form = {
+            name: '',
+            country: '',
+            city: '',
+            address: '',
+            type: '',
+            describe: '',
+          };
+          if (this.title == '编辑') {
+            this.form = this.editData;
+          }
         }
       },
-      watch:{
-        addShow(val){
-          this.dialogFormVisible = val;
-        },
-      },
-      props:{
-        addShow:{},
-        title:{}
-      }
+    },
+    props: {
+      addShow: {},
+      title: {},
+      editData: {}
     }
+  }
 </script>
 
 <style scoped>
--- src/page/organization/workplace/index.vue
+++ src/page/organization/workplace/index.vue
@@ -1,78 +1,115 @@
 <template>
-    <div>
-      <el-button type="primary" size="mini" @click="add">新增</el-button>
-      <el-button type="primary" size="mini">导入</el-button>
-      <el-button type="primary" size="mini">导出</el-button>
-      <el-table
-        :data="tableData"
-        border
-        class="table">
-        <el-table-column
-          v-for="(title,index) in tableDataTitle"
-          :key="index"
-          :type="title.type"
-          :prop="title.prop"
-          :label="title.label"
-          :width="title.width"
-          align="center">
-        </el-table-column>
-        <el-table-column
-          label="操作"
-          align="center">
-          <template  scope="scope">
-            <el-button type="primary" size="mini">编辑</el-button>
-            <el-button type="danger" size="mini">删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-      <addPlace :addShow="addShow" :title="title" @dialogClose="dialogClose" @submit="submit"></addPlace>
-    </div>
+  <div>
+    <el-button type="primary" size="mini" @click="add">新增</el-button>
+    <el-button type="primary" size="mini">导入</el-button>
+    <el-button type="primary" size="mini">导出</el-button>
+    <el-table
+      :data="tableData"
+      border
+      class="table">
+      <el-table-column
+        v-for="(title,index) in tableDataTitle"
+        :key="index"
+        :type="title.type"
+        :prop="title.prop"
+        :label="title.label"
+        :width="title.width"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        label="操作"
+        align="center">
+        <template scope="scope">
+          <el-button type="primary" size="mini" @click="edit(scope.row)">编辑</el-button>
+          <el-button type="danger" size="mini" @click="deleteData(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <addPlace :addShow="addShow" :editData = "editData" :title="title" @dialogClose="dialogClose" @submit="submit"></addPlace>
+  </div>
 </template>
 
 <script>
   import addPlace from './addPlace'
-    export default {
-        name: "index",
-      data(){
-          return{
-            tableData:[
-              {name:'深圳',country:'中国',city:'深圳',address:'福田',type:'注册地址',describe:'描述1'}
-            ],
-            tableDataTitle:[
-              {type:'selection',width:60},
-              {prop:'name',label:'名称',width:180},
-              {prop:'country',label:'国家/地区',width:180},
-              {prop:'city',label:'城市',width:120},
-              {prop:'address',label:'详细地址',width:180},
-              {prop:'type',label:'类型',width:180},
-              {prop:'describe',label:'描述',width:180},
-            ],
-            addShow:false,
-            title:'',
-          }
-      },
-      methods:{
-          add(){
-            this.title = '新增';
-            this.addShow = true;
-          },
-          dialogClose(){
-            this.addShow = false;
-          },
-        submit(data){
-            console.log(data)
-            this.tableData.push(data);
-        }
-      },
-      components:{
-        addPlace
+
+  export default {
+    name: "index",
+    data() {
+      return {
+        tableData: [
+          {id:1,name: '深圳', country: '中国', city: '深圳', address: '福田', type: '注册地址', describe: '描述1'}
+        ],
+        tableDataTitle: [
+          {type: 'selection', width: 60},
+          {prop: 'name', label: '名称', width: 180},
+          {prop: 'country', label: '国家/地区', width: 180},
+          {prop: 'city', label: '城市', width: 120},
+          {prop: 'address', label: '详细地址', width: 180},
+          {prop: 'type', label: '类型', width: 180},
+          {prop: 'describe', label: '描述', width: 180},
+        ],
+        addShow: false,
+        title: '',
+        editData:{}
       }
+    },
+    methods: {
+      add() {
+        this.title = '新增';
+        this.editData = {};
+        this.addShow = true;
+      },
+      edit(data) {
+        this.title = '编辑';
+        this.editData =  data;//克隆
+        this.addShow = true;
+      },
+      deleteData(data) {
+        this.$confirm('此操作将永久删除该数据, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.tableData.forEach((res,index)=>{
+            if(res.id==data.id){
+              this.tableData.splice(index,1)
+            }
+          })
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });
+        });
+      },
+      dialogClose() {
+        this.addShow = false;
+      },
+      submit(data,type) {
+        if(type==1){
+          this.tableData.push(data);
+        }else if(type==2){
+          this.tableData.forEach(res=>{
+            if(res.id==data.id){
+              Object.assign(res,data)//浅拷贝
+            }
+          })
+        }
+      }
+    },
+    components: {
+      addPlace
     }
+  }
 </script>
 
 <style scoped>
-.table{
-  width: 100%;
-  margin-top: 10px;
-}
+  .table {
+    width: 100%;
+    margin-top: 10px;
+  }
 </style>
